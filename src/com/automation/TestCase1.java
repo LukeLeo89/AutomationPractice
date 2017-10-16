669cbad1846d54aef9d468251cfbe8f4e9c2610b
Luke Sam <Luke.Sam@cubelogic.local>
2017-10-16 19:49:37 +0530

    Refactoring build.xml

--- src/com/automation/TestCase1.java
+++ src/com/automation/TestCase1.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import generic.Config;
@@ -45,5 +46,10 @@ public class TestCase1 {
 		}
 		
 		
-	}	
+	}
+	
+	@AfterSuite
+	public void closeBrowser(){
+		driver.close();
+	}
 }
